ac1793c9f668a564c46f97b13de0732ee35ba9d3
anagstef <anagstef@gmail.com>
2022-12-08 02:34:18 +0200

    Fix config

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  future: {},
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverBuildTarget: 'vercel'
 };
